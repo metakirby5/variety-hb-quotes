51d943172423116dd01dcff5649a9353cafda13d
Ethan Chan <metakirby5@gmail.com>
2015-04-03 20:48:05 -0700

    it works

--- hb_quotes.py
+++ hb_quotes.py
@@ -1,6 +1,6 @@
 from variety.plugins.IQuoteSource import IQuoteSource
 from gettext import gettext as _
-from urllib2 import urlopen, Request
+from urllib2 import urlopen, Request, HTTPError, URLError
 from urllib import quote as escape
 import logging
 import itertools
@@ -8,12 +8,13 @@ import json
 
 logger = logging.getLogger("variety")
 
-class IHBQuotes(IVarietyPlugin):
+class IHBQuotes(IQuoteSource):
 
   @classmethod
   def get_info(cls):
     return {
       "name": "Hummingbird.me",
+      "version": "0.1",
       "description": _("Grabs quotes from Hummingbird.me.\n"
                        "Keyword search is by anime_id. If none provided,"
                        "Monogatari Series quotes are used."
@@ -21,7 +22,7 @@ class IHBQuotes(IVarietyPlugin):
                        "  monogatari-series-second-season\n\n"
                        "No support for author search."),
       "author": "Ethan Chan",
-      "version": "0.1"
+      "url": "https://github.com/metakirby5/variety-hb-quotes",
     }
 
   REQ_URL = "https://hummingbird.me/quotes?anime_id=%s"
@@ -29,13 +30,6 @@ class IHBQuotes(IVarietyPlugin):
     'Accept': 'application/json, text/javascript, */*; q=0.01',
     'X-Requested-With': 'XMLHttpRequest',
   }
-  # A list with as many entries as favorite counts
-  FORMAT_ENTRY = lambda q: [{
-    'quote': q['content'],
-    'author': q['character_name'],
-    'sourceName': 'Hummingbird',
-    'link': "https://hummingbird.me/anime/%s/quotes" % q['anime_id'],
-  }] * q['favorite_count']
 
   DEFAULT_SERIES = [
     'bakemonogatari',
@@ -48,16 +42,34 @@ class IHBQuotes(IVarietyPlugin):
   # JSON request to API
   def quote_json(self, anime_id):
     # Make series safe to search for
-    anime_id = escape(anime_id, '')
-    req = Request(REQ_URL % anime_id, headers=REQ_HEADERS)
-    with urlopen(req) as f:
-      return json.loads(f.read())['quotes']
+    anime_id = escape(anime_id.strip(), '')
+    req = Request(self.REQ_URL % anime_id, headers=self.REQ_HEADERS)
+    f = urlopen(req)
+    quotes = json.loads(f.read())['quotes']
+    f.close()
+    logger.info('Got quotes for anime_id: %s' % anime_id)
+    return quotes
+
+  # Formats from HB to Variety
+  # Returns a list with as many entries as favorite counts
+  def format_entry(self, q):
+    return [{
+      'quote': q['content'],
+      'author': q['character_name'],
+      'sourceName': 'Hummingbird',
+      'link': "https://hummingbird.me/anime/%s/quotes" % q['anime_id'],
+    }] * q['favorite_count']
 
   # Returns a list of series quotes, multiplicity=favorite_count
   def get_series_quotes(self, anime_id):
     try:
-      return itertools.chain(*map(FORMAT_ENTRY, quote_json(anime_id)))
+      return itertools.chain(*map(self.format_entry,
+                                  self.quote_json(anime_id)))
+    except HTTPError:
+      logger.error("No series found matching anime_id: %s" % anime_id)
+      return []
     except URLError:
+      logger.error('No internet connection')
       return []
 
   #####
@@ -74,8 +86,10 @@ class IHBQuotes(IVarietyPlugin):
       return
     super(IHBQuotes, self).activate()
 
-    self.default_quotes =
-      itertools.chain(*[get_series_quotes(s) for s in DEFAULT_SERIES])
+    self.default_quotes = \
+      itertools.chain(*[self.get_series_quotes(s)
+                        for s in self.DEFAULT_SERIES])
+    logger.info('Created default quotes for %s' % self.DEFAULT_SERIES)
 
   def deactivate(self):
     super(IHBQuotes, self).deactivate()
@@ -86,5 +100,5 @@ class IHBQuotes(IVarietyPlugin):
 
   # Keyword = series
   def get_for_keyword(self, keyword):
-    return get_series_quotes(keyword)
+    return self.get_series_quotes(keyword)
 
