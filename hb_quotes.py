72eca5a1a42e27c22730c0bbd183dc1714ae509a
Ethan Chan <metakirby5@gmail.com>
2015-04-03 21:07:09 -0700

    better description

--- hb_quotes.py
+++ hb_quotes.py
@@ -16,10 +16,11 @@ class IHBQuotes(IQuoteSource):
       "name": "Hummingbird.me",
       "version": "0.1",
       "description": _("Grabs quotes from Hummingbird.me.\n\n"
-                       "Keyword search is by anime_id. If none provided,"
+                       "Tag search is by anime_id. If none provided,"
                        "Monogatari Series quotes are used.\n"
                        "Example:\n"
                        "    monogatari-series-second-season\n\n"
+                       "Quotes are weighted by favorite_count.\n"
                        "No support for author search."),
       "author": "Ethan Chan",
       "url": "https://github.com/metakirby5/variety-hb-quotes",
